fcf15e78a38e110b7afbed54f7eb4c8e4f546b77
Saadi <msaadali494@gmail.com>
2025-03-09 06:36:00 +0000

    conversion of datatypes

---
+++ 01-basics/03-conversion-operation.js
@@ -0,0 +1,33 @@
+let score = "saad"
+
+// console.log(typeof score);
+// console.log(typeof (score) );
+
+
+let valueInNumber = Number(score)
+// console.log( typeof valueInNumber);
+ // console.log(valueInNumber);
+
+
+
+
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1; false => 0
+
+
+let isLoggedIn = "saad"
+
+let booleanisLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "saad" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
