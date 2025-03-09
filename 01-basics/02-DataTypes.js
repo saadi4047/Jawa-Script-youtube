1bce40079612b7257a10b2c670b57807fdcba789
Saadi <msaadali494@gmail.com>
2025-03-09 05:57:37 +0000

    datatypes

---
+++ 01-basics/02-DataTypes.js
@@ -0,0 +1,38 @@
+"use strict"; // treat all JS code as newer version
+
+//  alert ( 3 + 3 ) // we are using nodejs not browser
+
+console.log( 3
+     + 3 ); // code readability should be high
+
+console.log( "saad" )
+
+let name = "saad"
+let age = 21
+let isLoggedIn = true
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false 
+// null =>  standalone value
+// undefined => 
+// symbols => unique
+
+
+// object
+
+
+console.log(typeof "saad");
+console.log(typeof "age");
+
+
+console.log(typeof  age);
+
+
+
+console.log(typeof null);  // => object
+console.log(typeof undefined);  // => undefined
+
+
